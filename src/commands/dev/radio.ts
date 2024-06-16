766fea41d6db7678b25902acc0b0d83c74eaeb7e
Ricardo Rocha <me@rrocha.uk>
2024-06-16 15:05:10 +0100

    more updates

--- src/commands/dev/radio.ts
+++ src/commands/dev/radio.ts
@@ -4,19 +4,22 @@ import { parseString } from 'xml2js';
 
 export class Radio extends Command {
   readonly isDevCommand: boolean = true;
-  
+
   async execute(): Promise<void> {
     const response = await fetch(
       'https://playerservices.streamtheworld.com/api/livestream?station=RFM&transports=http%2Chls&version=1.10',
     );
-    console.log(await response.text());
-    // parseString(await response.text(), (err, result) => {
-    //   if (err) {
-    //     return console.log(err);
-    //   }
-    //   console.log(result.live_stream_config.mountpoints[0].mountpoint[0].status);
-    // });
-    
+    parseString(await response.text(), (err, result) => {
+      if (err) {
+        return console.log(err);
+      }
+      const servers =
+        result.live_stream_config.mountpoints[0].mountpoint[0].servers[0].server;
+      const randomServerIdx = servers[Math.floor(Math.random() * servers.length)];
+      
+      console.log(servers[randomServerIdx]);
+    });
+
     this._interaction.reply('hello from radio dev');
   }
 
